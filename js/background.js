e2001bfaca3ee8efdf620ee86e1f6e3d3dbad958
Amanda Zimmerman <amanda.zimms@gmail.com>
2021-10-29 16:00:33 -0500

    secondary timer is working crudely: when user clicks 'take a break' or closes popup, a 20 second timer begins. After that ends, we start back at square 1. The handshakes seem to be slightly out of sync for this, as DOM elements are displaying incorrectly fir the first second of the break

--- js/background.js
+++ js/background.js
@@ -1,40 +1,54 @@
-let lookTimer = 0;
-let timerID;
-setLookTimer();
+let countdown = 0; //countown - when finished, time to either start or stop looking
+let isTakingBreak = false; //BREAK, throughout, refers to looking away, e.g. practicing 20-20-20
 
+const timeBetweenBreaks = 10; //how long between looks - 20 minutes = 1200 seconds
+const breakDuration = 5; //how long to look - 20 seconds
 
-function setLookTimer(){
-  console.log('setting look timer');
-  clearInterval(timerID);
+let countdownID; //used to start and stop countdowns
 
-  chrome.notifications.clear('timeToLook');
+setCountdownTilBreak();
 
-  lookTimer = 4;
+//PHASE 1: START the first timer, WAIT til user is ready to look away from the screen
+function setCountdownTilBreak(){
+  //function that counts down until time to look every other time.
+  
+  //start by initializing timer, clearing notifications and previously running timers
+  clearInterval(countdownID);
+  chrome.notifications.clear('timeToBreak');
+  countdown = timeBetweenBreaks;
+  isTakingBreak = false;
+  
+  //using this setup to SEND isTakingBreak to popup
+  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.method == "isBreakStatus") {
+        sendResponse({ method: "", data: isTakingBreak }) 
+    }
+  })
 
-  timerID = setInterval(() => { 
-   
-    console.log ('in setInterval. lookTimer is', lookTimer);
-    if (lookTimer > 0) {
-      lookTimer--;
+  countdownID = setInterval(() => { //countdown til 0, then make a notification
+    
+    console.log ('in setInterval. time til next break is', countdown);
+    if (countdown > 0) {
+      countdown--;
     }
     else {
-      makeTimeToLookNotification();
-      clearInterval(timerID);
+      makeBreakNotification();
+      clearInterval(countdownID);
       return;
     }
 
-    //using this setup to SEND lookTimer to popup
+    //using this setup to SEND timeUntilNextLook to popup for display there
     chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-      if (request.method == "lookTimerStatus") {
-          sendResponse({ method: "", data: lookTimer }) 
+      if (request.method == "timeTilBreakStatus") {
+          sendResponse({ method: "", data: countdown }) 
       }
     })
-
-  }, 1000);
+  }, 1000); //runs every 1 second
 }
 
-function makeTimeToLookNotification(){
-  chrome.notifications.create('timeToLook', {
+//PHASE 2: MAKE a "take a break" notification for user
+function makeBreakNotification(){
+  chrome.notifications.create('timeToBreak', {
     type: 'basic',
     iconUrl: '/Eye128.png',
     title: 'Time to look!',
@@ -43,18 +57,55 @@ function makeTimeToLookNotification(){
   });
 }
  
-chrome.notifications.onClosed.addListener(function(timeToLook) {
-  //runs when this notification is closed
-  console.log('Popup was closed.');
-  setLookTimer();
+//PHASE 3: WHEN for user indicates they're ready to take a break
+    //3a: 1st of 2 things that can mean user has begun looking: clicked to clear popup
+chrome.notifications.onClosed.addListener(function(timeToBreak) {
+  setCountdownTilScreen();
 });
 
-
-//using this setup to RECEIVE "true" from popup (re: click "take a break" = true)
+    //3b: 2nd of 2 things that can mean user has begun looking: clicked "take a break (early)"
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.method == "takeBreakStatus" && request.data === true) {
-      setLookTimer();
-
+  //using this setup to RECEIVE "true" from popup (re: click "take a break" = true)
+  if (request.method == "takeBreakStatus" && request.data === true) { 
+    setCountdownTilScreen();
       sendResponse({ method: "", data: "" })
   }
 });
+
+//PHASE 4: START countown until screen time (i.e. until break is over)
+function setCountdownTilScreen(){
+  //function that counts down until time to stop looking away
+  
+  //start by initializing timer, clearing notifications and previously running timers
+  clearInterval(countdownID);
+  countdown = breakDuration;
+  isTakingBreak = true;
+
+  //using this setup to SEND isTakingBreak to popup
+  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.method == "isBreakStatus") {
+        sendResponse({ method: "", data: isTakingBreak }) 
+    }
+  })
+
+  countdownID = setInterval(() => { //countdown til 0, then reset the timer to 20
+    
+    console.log ('in setInterval. time til back to work is', countdown);
+    if (countdown > 0) {
+      countdown--;
+    }
+    else {
+      setCountdownTilBreak(countdownID);
+      return;
+    }
+
+    //using this setup to SEND countdown to popup for display there
+    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+      if (request.method == "timeTilBreakOverStatus") {
+          sendResponse({ method: isTakingBreak, data: countdown }) 
+      }
+    })
+  }, 1000); //runs every 1 second
+}
+
+
--- js/popup.js
+++ js/popup.js
@@ -1,36 +1,68 @@
 let outputArea = $('#outputArea');
-let timerTag = $('#timerTag');
-let breakNow = $('#breakNow');
+let countdownTag = $('#countdownTag');
+let breakButton = $('#breakButton');
 
 $( document ).ready( function(){
-  checkTime();
-  breakNow.on('click', takeBreak);
+  checkStatus();
+  breakButton.on('click', takeBreak);
 });
 
-function takeBreak(){
-  console.log('break');
-  
+function takeBreak(){ 
+  //runs when user clicks "take a break (early)"
+
   //using this setup to SEND "true" to background (re: click "take a break" = true)
   chrome.runtime.sendMessage({ method: "takeBreakStatus", data: true }, function (res) {
     return true;
   });
 }
 
-function checkTime(){ 
-  
-  setInterval(() => { 
-    breakNow.text("Take A Break Early");
-  
-    //using this to RECEIVE lookTimer from background
-    chrome.runtime.sendMessage({ method: "lookTimerStatus", data: "" }, function (res) { 
-      let currentTimer = res.data
+function checkStatus(){ 
+  //continually running (2 per second) function that fetches countdown from background.js
 
-      if (currentTimer > 0)
-        timerTag.text(`Time until next break: ${currentTimer}`);
-      else  {
-        timerTag.text(`It's time for 20-20-20`);
-        breakNow.text("Take A Break");
-      }
+  let isTakingBreak;
+  setInterval(() => { 
+  
+    //using this setup to RECEIVE isTakingBreak from background
+    chrome.runtime.sendMessage({ method: "isBreakStatus", data: "" }, function (res) { 
+      isTakingBreak = res.data
     }); 
+
+    if(!isTakingBreak) {
+      console.log('not breaking');
+      breakButton.show(); //if we're not taking a break, show the 'take a break' button
+      breakButton.text("Take A Break Early"); //if it's not time to take a break yet, update this button wording
+      
+      //using this setup to RECEIVE timeTilBreak from background
+      chrome.runtime.sendMessage({ method: "timeTilBreakStatus", data: "" }, function (res) { 
+        let currentTimer = res.data
+
+        if (currentTimer > 0)
+          countdownTag.text(`Time until next break: ${currentTimer}`);
+        else  {
+          countdownTag.text(`It's time for 20-20-20!`);
+          breakButton.text("Take A Break"); //change the button wording to remove 'early'
+        }
+      }); 
+    }
+
+    else {
+      console.log('BREAKTIME');
+
+      breakButton.hide(); //if we're already taking a break, hide this button 
+      
+      //using this setup to RECEIVE timeTilBreakOver from background
+      chrome.runtime.sendMessage({ method: "timeTilBreakOverStatus", data: "" }, function (res) { 
+        let currentTimer = res.data
+
+        if (currentTimer > 0)
+          countdownTag.text(`Keep looking for: ${currentTimer}`);
+        else  {
+          countdownTag.text(`Great work! You can now get back to work :)`);
+          breakButton.hide();
+        }
+      }); 
+    }
+
   }, 500); 
 }
+
